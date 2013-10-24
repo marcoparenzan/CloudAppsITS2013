66b9d140f0a77062f4db57a29a3657608e98fa17
marco.parenzan <marco.parenzan@libero.it>
2013-10-24 06:02:30 +0200

    ConsoleApp
    Riusa lo stesso codice anche in altre applicazioni...

---
+++ ConsoleApp/ListListino.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Company.DomainModels;
+
+namespace ConsoleApp
+{
+    public class ListListino: IRepository<Articolo>
+    {
+        private List<Articolo> _listino = new List<Articolo>();
+
+        private List<Articolo> Listino
+        {
+            get
+            {
+                return _listino;
+            }
+        }
+
+        public void Add(Articolo item)
+        {
+            item.ArticoloId =
+                Listino
+                // .Where(ArticoloMaggioreDiZero)
+                .DefaultIfEmpty(new Articolo
+                {
+                    ArticoloId = 0
+                })
+                .Select(xx => xx.ArticoloId)
+                .Max() + 1;
+
+            Listino.Add(item);
+        }
+
+        public void Update(Articolo item)
+        {
+            var existing = Listino.SingleOrDefault(xx => xx.ArticoloId == item.ArticoloId);
+            if (existing != null) Listino.Remove(item);
+            Listino.Add(item);
+        }
+
+        public IQueryable<Articolo> Items
+        {
+            get { return Listino.AsQueryable(); }
+        }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Company.DomainModels;
+using Company.ReadModels;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IRepository<Articolo> repository = new ListListino();
+            var aggregateRoot = new ListinoAggregateRoot(repository);
+            var dataContext = new ListinoDataContext(repository);
+            List(dataContext);
+            aggregateRoot.CreaArticolo("1", "AAA", 50, true, false);
+            aggregateRoot.CreaArticolo("2", "BBB", 20, true, false);
+            aggregateRoot.CreaArticolo("3", "CCC", 10, true, false);
+            aggregateRoot.CreaArticolo("4", "DDD", 50, true, false);
+            List(dataContext);
+
+            Console.ReadLine();
+        }
+
+        static void List(ListinoDataContext dc)
+        {
+            var page = dc.Page(0, 10);
+
+            Console.WriteLine("ArticoloId;Descrizione");
+            foreach (var item in page.Items)
+            {
+                Console.WriteLine("{0};{1}", item.ArticoloId, item.Descrizione);
+            }
+        }
+    }
+}
